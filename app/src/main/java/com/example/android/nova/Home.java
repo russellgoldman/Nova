ee64460a8ea5abb2557531cee6f73259fceb2674
russellgoldman <russellgoldman@Russells-MacBook-Air.local>
2018-03-04 03:03:32 -0500

    Finished layout for Home in Android Studio

--- app/src/main/java/com/example/android/nova/Home.java
+++ app/src/main/java/com/example/android/nova/Home.java
@@ -3,6 +3,7 @@ package com.example.android.nova;
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 
 public class Home extends AppCompatActivity {
 
